34eb0b64387b13fd3c0fe74264ca2afb2e8f6c92
Edward Zhou <Edward_mjz@hotmail.com>
2019-07-13 13:56:56 +1000

    Extract tabletop module

--- lib/edward_robot.ex
+++ lib/edward_robot.ex
@@ -2,10 +2,9 @@ defmodule EdwardRobot do
   @moduledoc false
   @type word() :: String.t()
   defstruct x: 0, y: 0, direction: nil
+  import Tabletop
 
   @valid_directions Direction.directions()
-  @valid_x 0..5
-  @valid_y 0..5
 
   @doc """
   Initialize Edward Robot, create a robot pid to store state
@@ -16,13 +15,9 @@ defmodule EdwardRobot do
 
   @doc """
   Place robot on the tabletop
-
-  valid x value is within 0 -> 5
-  valid y value is within 0 -> 5
-  Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec place(direction :: atom, x :: integer, y :: integer) :: atom | {:error, word()}
-  def place(direction, x, y) when x in @valid_x and y in @valid_y and direction in @valid_directions  do
+  def place(direction, x, y) when valid_x?(x) and valid_y?(y) and direction in @valid_directions  do
     Agent.update(:edward_robot, & &1 = %EdwardRobot{x: x, y: y, direction: direction})
   end
 
@@ -33,6 +28,8 @@ defmodule EdwardRobot do
   """
   @spec left() :: atom
   def left do
+    robot = get_robot()
+
     update_robot(robot.x, robot.y, Direction.left_from(robot.direction))
   end
 
@@ -41,6 +38,8 @@ defmodule EdwardRobot do
   """
   @spec right() :: atom
   def right do
+    robot = get_robot()
+
     update_robot(robot.x, robot.y, Direction.right_from(robot.direction))
   end
 
@@ -49,10 +48,12 @@ defmodule EdwardRobot do
   """
   @spec move() :: atom
   def move do
+    robot = get_robot()
+
     update_placement(robot.x, robot.y, robot.direction)
   end
 
-  defp robot do
+  defp get_robot do
     Agent.get(:edward_robot, & &1)
   end
 
---
+++ lib/tabletop.ex
@@ -0,0 +1,17 @@
+defmodule Tabletop do
+  @moduledoc false
+  @max_x 5
+  @max_y 5
+
+  defmacro valid_x?(x) do
+    quote do
+      unquote(x) in 0..unquote(@max_x)
+    end
+  end
+
+  defmacro valid_y?(y) do
+    quote do
+      unquote(y) in 0..unquote(@max_y)
+    end
+  end
+end
