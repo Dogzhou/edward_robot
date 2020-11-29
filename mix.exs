fadde3396b24360e29f7892e6911f577ec8d19ec
Edward Zhou <Edward_mjz@hotmail.com>
2020-11-29 15:27:37 +1100

    Add test coverage and exdoc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,25 @@ defmodule EdwardRobot.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      # Docs
+      name: "Edward Poker",
+      source_url: "https://github.com/edxzh/edward_robot",
+      docs: [
+        # The main page in the docs
+        main: "Edward Robot",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -23,6 +41,8 @@ defmodule EdwardRobot.MixProject do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
